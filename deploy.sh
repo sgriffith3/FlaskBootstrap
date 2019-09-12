c422d15363d608137729303e381196d56467245b
Sam Griffith <47530477+sgriffith3@users.noreply.github.com>
2019-09-11 22:03:00 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
-SET -e
+set -e
 apt update
 apt install python3-venv python3-pip -y
 mkdir /var/log/<project_name>
 addgroup <project_name>
 useradd -g <project_name> <project_name>
 
-
-# make the three directories of the current live site (src), backup duirng deploy (backup) and staged files for deployment (staging)
+# make the directories of the current live site (src)
 
 mkdir -p /opt/<project_name> /opt/<project_name>/src 
 # Copy project to /opt/<project_name>/src/
+cp -r * /opt/<project_name>/src
+cp requirements.txt /opt/<project_name>/
+cp <project_name>.logging.yaml /opt/<project_name>/
 
 chown -R <project_name>:<project_name> /opt/<project_name>
 sudo -u <project_name> bash -c "python3 -m venv /opt/<project_name>/venv"
 sudo -u <project_name> bash -c "/opt/<project_name>/venv/bin/pip install -r /opt/<project_name>/src/requirements.txt --no-cache"
 
-cp /opt/<project_name>/src/<project_name>.service /etc/systemd/system/<project_name>.service
+cp <project_name>.service /etc/systemd/system/<project_name>.service
 chown root:root /etc/systemd/system/<project_name>.service
 chmod 0755 /etc/systemd/system/<project_name>.service
 systemctl daemon-reload
