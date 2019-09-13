ffa86f001fa3039993722057e5107ab2f18bc984
Sam Griffith <47530477+sgriffith3@users.noreply.github.com>
2019-09-12 20:20:47 -0400

    Update templated.py

--- project_name/views/templated.py
+++ project_name/views/templated.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 import logging
-
+import subprocess
 from flask import render_template, Blueprint
 
-from project_name.config import config
+from nodifyer.config import config
+
 
 blueprint = Blueprint('templated', __name__, template_folder='templates')
 
-log = logging.getLogger('<project_name>')
+log = logging.getLogger('NodifyerAPI')
 
 
 @blueprint.route('/')
-@blueprint.route('/index')
-def index():
-    return render_template('index.html',
-                           page_name='Main',
-                           project_name="<project_name>")
+@blueprint.route('/info')
+def return_info():
+    # Gathers info of VMs
+    output = subprocess.check_output('ps aux | grep qemu-system', shell=True)
+    output = output.decode('utf-8')
+    output = output.split('\n')
+    node = {}
+    for item in output:
+        vm_name, cpus, mem = 'vm_name', '0', '0'
+        if "file=" in item:
+            image = item.find("file=") + 5
+            image = item[image:image+40].split("/")
+            vm_name = image[-1].split(',')[0]
+        if "cpus=" in item:
+            cpu = item.find("cpus=") + 5
+            cpus = item[cpu]
+        if "-m " in item:
+            mem = item.find("-m ") + 3
+            mem = item[mem:mem+4]
+        vm = {vm_name: {"cores": cpus, "memory": mem}}
+        node.update(vm)
+    return node
